2e5a63f9489485f60d31c05599fb2759bc002a8a
Jonathan Oakes <jonathan@solaris-apps.co.uk>
2025-01-22 22:41:16 +0000

    added cats name to add / edit cat button

--- src/components/cat-details-form/upload-cat-details-button.component.jsx
+++ src/components/cat-details-form/upload-cat-details-button.component.jsx
@@ -4,8 +4,11 @@ import { Button } from "../../styles/button/button.styles";
 import { BlackHr } from "../../styles/hr/hr.styles";
 
 import { addCatRoute } from "../../strings/routes";
+import useGetCatDetailsManagementSelectors from "../../hooks/selectors/use-get-cat-details-management-selectors";
 
 const UploadCatDetailsButton = () => {
+  const { catsName } = useGetCatDetailsManagementSelectors();
+
   const location = useLocation();
   const path = location.pathname;
 
@@ -13,7 +16,7 @@ const UploadCatDetailsButton = () => {
     <>
       <BlackHr />
       <Button type="submit" className="submit">
-        {path === addCatRoute ? "add" : "update"} cat
+        {path === addCatRoute ? "add" : "update"} {catsName ? catsName : "cat"}
       </Button>
     </>
   );
