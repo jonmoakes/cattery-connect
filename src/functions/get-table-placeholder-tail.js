c44ff3877bde80695d9ac2188105021a7cfc5818
Jonathan Oakes <jonathan@solaris-apps.co.uk>
2025-01-23 21:56:29 +0000

    added placeholder tail for all cats route

--- src/functions/get-table-placeholder-tail.js
+++ src/functions/get-table-placeholder-tail.js
@@ -1,10 +1,15 @@
-import { allCustomersRoute, viewCustomersCatsRoute } from "../strings/routes";
+import {
+  allCustomersRoute,
+  viewCustomersCatsRoute,
+  allCatsRoute,
+} from "../strings/routes";
 
 export const getTablePlaceholderTail = (path) => {
   switch (path) {
     case allCustomersRoute:
       return "Customers";
     case viewCustomersCatsRoute:
+    case allCatsRoute:
       return "Cats";
     default:
       return "";
