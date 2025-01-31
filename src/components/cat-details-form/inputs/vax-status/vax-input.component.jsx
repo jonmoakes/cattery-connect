2883a523518f7c81942cda9ca7327623483847e4
Jonathan Oakes <jonathan@solaris-apps.co.uk>
2025-01-31 00:45:56 +0000

    changed vax status required logic

--- src/components/cat-details-form/inputs/vax-status/vax-input.component.jsx
+++ src/components/cat-details-form/inputs/vax-status/vax-input.component.jsx
@@ -1,26 +1,34 @@
+import { useLocation } from "react-router-dom";
 import { InnerFormDiv } from "../../../../styles/div/div.styles";
 import { Label } from "../../../../styles/form/form.styles";
 
 import { vaccinationRadioChoices } from "../vaccination-radio-choices";
 
-const VaxInput = ({ handleCatDetailsChange }) => (
-  <>
-    {vaccinationRadioChoices.map((choice) => {
-      const { id, label, value } = choice;
-      return (
-        <InnerFormDiv key={id}>
-          <Label className="no-padding">{label}</Label>
-          <input
-            type="radio"
-            name="vaccinationStatus"
-            value={value}
-            onChange={handleCatDetailsChange}
-            required
-          />
-          <hr />
-        </InnerFormDiv>
-      );
-    })}
-  </>
-);
+import { editCatRoute } from "../../../../strings/routes";
+
+const VaxInput = ({ handleCatDetailsChange }) => {
+  const location = useLocation();
+  const path = location.pathname;
+
+  return (
+    <>
+      {vaccinationRadioChoices.map((choice) => {
+        const { id, label, value } = choice;
+        return (
+          <InnerFormDiv key={id}>
+            <Label className="no-padding">{label}</Label>
+            <input
+              type="radio"
+              name="vaccinationStatus"
+              value={value}
+              onChange={handleCatDetailsChange}
+              required={path !== editCatRoute ? true : false}
+            />
+            <hr />
+          </InnerFormDiv>
+        );
+      })}
+    </>
+  );
+};
 export default VaxInput;
