d1cfe56b1edd88e9b3e534df4a6d98b62548025a
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2025-03-04 14:34:42 +0000

    removed with from container

--- src/styles/container/container.styles.jsx
+++ src/styles/container/container.styles.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100%;
   margin-top: 125px;
   padding-bottom: 100px;
   @media screen and (max-width: 600px) {
