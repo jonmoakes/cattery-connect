df81bc6f78489d3c673b7013b759c8642cccadc5
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2025-04-01 12:30:44 +0100

    added version to footer

--- src/components/footer/copyright.component.jsx
+++ src/components/footer/copyright.component.jsx
@@ -1,13 +1,16 @@
 import CustomBalancedText from "../custom-balanced-text/custom-balanced-text.component";
 
 const Copyright = () => (
-  <CustomBalancedText>
-    Copyright ©{" "}
-    <a target="_blank" rel="noreferrer" href="https://www.solaris-apps.co.uk">
-      Solaris Apps
-    </a>{" "}
-    2025
-  </CustomBalancedText>
+  <>
+    <CustomBalancedText>
+      Copyright ©{" "}
+      <a target="_blank" rel="noreferrer" href="https://www.solaris-apps.co.uk">
+        Solaris Apps
+      </a>{" "}
+      2025
+    </CustomBalancedText>
+    <CustomBalancedText>V 1.0.0</CustomBalancedText>
+  </>
 );
 
 export default Copyright;
